9b9af15bd5b20b59fc4fa3b8afc1aea6a0d3fb85
stevenyan <steven.yan42@gmail.com>
2024-08-18 22:19:01 -0700

    Completed submit and edit logic for Personal Info Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import PersonalCard from './components/Personal'
 
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+});
+
 
   return (
     <>
-      <PersonalCard />
+      <PersonalCard handlePersonalSubmit={setPersonalInfo}/>
+      {/* for test */}
+      <h2>{personalInfo.firstName}</h2>
+      <h2>{personalInfo.lastName}</h2>
+      <h2>{personalInfo.email}</h2>
+      <h2>{personalInfo.phone}</h2>
     </>
   )
 }
--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 
-function PersonalCard() {
+function PersonalCard({handlePersonalSubmit}) {
+    
     const [personalInfo, setPersonalInfo] = useState({
         firstName: "",
         lastName: "",
@@ -9,44 +10,100 @@ function PersonalCard() {
         phone: "",
     });
 
+    //to enable editing
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    //controlled input for text
     const handleInputChange = (event) => {
         const { id, value } = event.target;
         setPersonalInfo((prevInfo) => 
         ({ ...prevInfo, [id]: value }));
     }
 
-    return (
-        <div className="card">
-            <label htmlFor="firstName">First Name:</label>
-            <input 
-                type="text"
-                value={personalInfo.firstName}
-                id="firstName"
-                onChange={handleInputChange}
-            />
-            <label htmlFor="lastName">Last Name:</label>
-            <input 
-                type="text"
-                value={personalInfo.lastName}
-                id="lastName"
-                onChange={handleInputChange}
-            />
-            <label htmlFor="email">Email:</label>
-            <input 
-                type="email"
-                value={personalInfo.email}
-                id="email"
-                onChange={handleInputChange}
-            />
-            <label htmlFor="phone">Phone Number:</label>
-            <input 
-                type="tel"
-                value={personalInfo.phone}
-                id="phone"
-                onChange={handleInputChange}
-            />
-        </div>
-    );
-}
+    //pass personalInfo state on submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handlePersonalSubmit(personalInfo);
+        setIsSubmitted(true);
+    }
 
-export default PersonalCard
+    const handleEditClick = () => {
+        setIsSubmitted(false);
+    }
+
+    return (
+        <form className="card" action="" onSubmit={handleSubmit}>
+            {isSubmitted ? (
+                <div>
+                    <h2>Personal Info:</h2>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input 
+                        type="text"
+                        value={personalInfo.firstName}
+                        id="firstName"
+                        readOnly
+                    />
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input 
+                        type="text"
+                        value={personalInfo.lastName}
+                        id="lastName"
+                        readOnly
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="email"
+                        value={personalInfo.email}
+                        id="email"
+                        readOnly
+                    />
+                    <label htmlFor="phone">Phone Number:</label>
+                    <input 
+                        type="tel"
+                        value={personalInfo.phone}
+                        id="phone"
+                        readOnly
+                    />
+                    <button type="submit" disabled>Submit</button>
+                    <button onClick={handleEditClick}>Edit</button>
+                </div>
+                ):(
+                <div>
+                    <h2>Personal Info:</h2>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input 
+                        type="text"
+                        value={personalInfo.firstName}
+                        id="firstName"
+                        onChange={handleInputChange}
+                    />
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input 
+                        type="text"
+                        value={personalInfo.lastName}
+                        id="lastName"
+                        onChange={handleInputChange}
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="email"
+                        value={personalInfo.email}
+                        id="email"
+                        onChange={handleInputChange}
+                    />
+                    <label htmlFor="phone">Phone Number:</label>
+                    <input 
+                        type="tel"
+                        value={personalInfo.phone}
+                        id="phone"
+                        onChange={handleInputChange}
+                    />
+                    <button type="submit">Submit</button>
+                    <button disabled>Edit</button>
+                </div>)
+            }
+        </form>
+    );
+};
+
+export default PersonalCard;
